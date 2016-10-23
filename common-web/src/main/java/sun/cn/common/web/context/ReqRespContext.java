d4a1a9b04b47d5a14db86839de24715a8fc1215e
sunshine <sunshine@JWC4Q9EMLNIGEC8>
2016-10-23 16:02:32 +0800

    commit

--- common-web/src/main/java/sun/cn/common/web/context/ReqRespContext.java
+++ common-web/src/main/java/sun/cn/common/web/context/ReqRespContext.java
@@ -4,37 +4,54 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 单线程共享request,response
+ * 上下文共享池
+ * 
  * @author sunshine
  * @date 2016年4月16日
  * @desc
  */
 public class ReqRespContext {
-	
+
 	private final static ThreadLocal<HttpServletRequest> requestLocal = new ThreadLocal<HttpServletRequest>();
-	
+
 	private final static ThreadLocal<HttpServletResponse> responseLocal = new ThreadLocal<HttpServletResponse>();
-	
-	private ReqRespContext(){}
-	
-	public static void set(HttpServletRequest request,HttpServletResponse response){
+
+	private ReqRespContext() {
+	}
+
+	public static void set(HttpServletRequest request,
+			HttpServletResponse response) {
 		requestLocal.set(request);
 		responseLocal.set(response);
 	}
-	
-	public static void setReq(HttpServletRequest value){
+
+	public static void setReq(HttpServletRequest value) {
 		requestLocal.set(value);
 	}
-	
-	public static HttpServletRequest getReq(){
-		return requestLocal.get();
+
+	public static HttpServletRequest getReq() {
+		try {
+			HttpServletRequest request = requestLocal.get();
+			return request;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			requestLocal.remove();
+		}
 	}
-	
-	public static void setResp(HttpServletResponse value){
+
+	public static void setResp(HttpServletResponse value) {
 		responseLocal.set(value);
 	}
-	
-	public static HttpServletResponse getResp(){
-		return responseLocal.get();
+
+	public static HttpServletResponse getResp() {
+		try {
+			HttpServletResponse response = responseLocal.get();
+			return response;
+		} catch (Exception e) {
+			return null;
+		} finally {
+			responseLocal.remove();
+		}
 	}
 }
